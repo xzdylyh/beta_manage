818192c9381052623498cf376fc255f98aaa9852
unknown <yechangchun.bj@acewill.cn>
2018-11-09 17:04:48 +0800

    add chargeList

---
+++ testCase/testchargeListPage.py
@@ -0,0 +1,160 @@
+import os
+import unittest
+import ddt
+from pages.chargeListPage import ChargeList
+from lib.scripts import (
+    select_Browser_WebDriver,
+    reply_case_fail,
+    get_data
+)
+from lib import (
+    gl,
+    HTMLTESTRunnerCN
+)
+
+
+@ddt.ddt
+class TestChargeListPage(unittest.TestCase):
+    """储值规则设置--门店规则"""
+    @classmethod
+    def setUpClass(cls):
+        cls.driver = select_Browser_WebDriver()
+        cls.url = 'http://manage.beta.acewill.net/charge/edit?type=shop' # 门店规则入口
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.quit()
+         # pass
+
+    # @unittest.skip('调试')
+    @ddt.data(*get_data('chargeListPage', 'CASE1'))
+    @reply_case_fail(num=3)
+    def testcase1(self,data):
+        """"创建储值规则-固定金额"""
+        self.clist = ChargeList(self.url, self.driver, data['title'])
+        # 打开创建储值规则页面
+        self.clist.open
+        # 输入规则名称
+        self.clist.inputRuleNameValue(data['name'])
+        # 输入充值金额
+        self.clist.inputPrepaidValue(data['charge'])
+        # 选择充值送的类型;0:固定金额，1：固定积分，2：代金券/礼品券
+        self.clist.selectStoreRule(data['storeRule'])
+        # 选择固定金额
+        self.clist.selectInputAccount(data['account'])
+        # 选择固定金额（元）/实收金额的送（%）
+        self.clist.inputPresentValue(data['presentValue'])
+        # 点击保存
+        self.clist.clictSubmitBtn()
+        # 断言是否保存成功
+        self.assertEqual(self.clist.getScuessText(), data['successText'])
+        # 点击返回
+        self.clist.clickBcakBtn()
+        # 点击删除
+        self.clist.clickDelBtn()
+        # 点击确定
+        self.clist.clickOkBtn()
+        # 验证删除成功
+        self.assertEqual(self.clist.getDelInfo(),data['delInfo'])
+
+
+    # @unittest.skip('调试')
+    @ddt.data(*get_data('chargeListPage', 'CASE2'))
+    @reply_case_fail(num=3)
+    def testcase2(self, data):
+        """"创建储值规则-固定积分"""
+        self.clist = ChargeList(self.url, self.driver, data['title'])
+        # 打开创建储值规则页面
+        self.clist.open
+        # 输入规则名称
+        self.clist.inputRuleNameValue(data['name'])
+        # 输入充值金额
+        self.clist.inputPrepaidValue(data['charge'])
+        # 选择充值送的类型;0:固定金额，1：固定积分，2：代金券/礼品券
+        self.clist.selectStoreRule(data['storeRule'])
+        # 选择固定积分
+        self.clist.selectInputIntegral(data['integra'])
+        # 选择固定金额（元）/实收金额的送（%）
+        self.clist.inputIntegralValue(data['integralValue'])
+        # 点击保存
+        self.clist.clictSubmitBtn()
+        # 断言是否保存成功
+        self.assertEqual(self.clist.getScuessText(), data['successText'])
+        # 点击返回
+        self.clist.clickBcakBtn()
+        # 点击删除
+        self.clist.clickDelBtn()
+        # 点击确定
+        self.clist.clickOkBtn()
+        # 验证删除成功
+        self.assertEqual(self.clist.getDelInfo(), data['delInfo'])
+
+
+
+    @ddt.data(*get_data('chargeListPage', 'CASE3'))
+    @reply_case_fail(num=3)
+    def testcase3(self, data):
+        """"创建储值规则-代金券/礼品券"""
+        self.clist = ChargeList(self.url, self.driver, data['title'])
+        # 打开创建储值规则页面
+        self.clist.open
+        # 输入规则名称
+        self.clist.inputRuleNameValue(data['name'])
+        # 输入充值金额
+        self.clist.inputPrepaidValue(data['charge'])
+        # 选择充值送的类型;0:固定金额，1：固定积分，2：代金券/礼品券
+        self.clist.selectStoreRule(data['storeRule'])
+        # 选择代金券/礼品券，0：代金券，1：礼品券
+        self.clist.selectGift(data['couponIndex'])
+        # 点击使用代金券,0：默认使用当前第一个券
+        self.clist.selectCoupon(data['couponValue'])
+
+
+        # 点击保存
+        self.clist.clictSubmitBtn()
+        # 断言是否保存成功
+        self.assertEqual(self.clist.getScuessText(), data['successText'])
+        # 点击返回
+        self.clist.clickBcakBtn()
+        # 点击删除
+        self.clist.clickDelBtn()
+        # 点击确定
+        self.clist.clickOkBtn()
+        # 验证删除成功
+        self.assertEqual(self.clist.getDelInfo(), data['delInfo'])
+
+
+
+
+
+if __name__ == "__main__":
+    # unittest.main(verbosity=2)
+
+   
+    suite = unittest.TestSuite()
+
+    tests = [
+        unittest.TestLoader().loadTestsFromTestCase(TestChargeListPage)
+    ]
+    suite.addTests(tests)
+    filePath = os.path.join(gl.reportPath, 'Report.html')  # 确定生成报告的路径
+    print(filePath)
+
+    with open(filePath, 'wb') as fp:
+        runner = HTMLTESTRunnerCN.HTMLTestRunner(
+            stream=fp,
+            title=u'UI自动化测试报告',
+            description=u'详细测试用例结果',  # 不传默认为空
+            tester=u"yecc"  # 测试人员名字，不传默认为小强
+        )
+        # 运行测试用例
+        runner.run(suite)
+
+
+
+
+
+
+
+
+
