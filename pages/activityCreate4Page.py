037b764dca8145ed397eb63e026f83b4233e657b
lengyaohui <lengyaohui.bj@acewill.cn>
2018-11-27 10:36:12 +0800

    增加给活跃会员赠券activityCreate4

---
+++ pages/activityCreate4Page.py
@@ -0,0 +1,7 @@
+from pages.activityCreate2Page import ActivityCreate2
+
+class ActivityCreate4(ActivityCreate2):
+    """给活跃会员赠券"""
+    #<<<<<<<<<<<<<<<<<<<<<<<定位<<<<<<<<<<<<<<<<<<<<<
+    pass
+    #<<<<<<<<<<<<<<<<<<<<<<<定位<<<<<<<<<<<<<<<<<<<<<
